29efaa38c0cfa4c9ba9ccd46d1238309946812bd
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-02-02 14:51:41 +0200

    Cancel button

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import Contact, {
 } from "./routes/contact";
 import EditContact, { action as editAction, } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
   const router = createBrowserRouter([
     {
@@ -22,6 +23,7 @@ import { action as destroyAction } from "./routes/destroy";
       loader: rootLoader,
       action: rootAction,
       children: [
+        { index: true, element: <Index /> },
         {
           path:"contacts/:contactId",
           element: <Contact />,
--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,4 +1,4 @@
-import { Form, useLoaderData, redirect } from "react-router-dom";
+import { Form, useLoaderData, redirect, useNavigate, } from "react-router-dom";
 import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
@@ -14,6 +14,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const { contact } = useLoaderData();
+  const navigate = useNavigate();
 
 
   return (
@@ -64,7 +65,14 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button
+         type="button"
+          onClick={() => {
+            navigate(-1);
+          }}
+          >
+            Cancel
+            </button>
       </p>
     </Form>
   );
